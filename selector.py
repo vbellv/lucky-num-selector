6559e50d489291cfd13d3ab2a8fa9a07613c0cb9
vbellv <belleb724@gmail.com>
2023-05-01 14:40:24 +0900

    Update selector.py

--- selector.py
+++ selector.py
@@ -4,8 +4,7 @@ def check_python():
     print('check python')
 
 def random_num():
-    result = random.sample(range(1, 46), k=6)
-    print(result)
+    return random.sample(range(1, 46), k=6)
 
 if __name__ == '__main__':
-    random_num()
+    print(random_num())
